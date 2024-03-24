26ee058835fd67b6d34b9da94f1d630df5a0bc34
Raj Rajhans <me@rajrajhans.com>
2024-03-24 15:51:11 +0530

    chore: mix project

---
+++ lib/elixir1brc.ex
@@ -0,0 +1,18 @@
+defmodule Elixir1brc do
+  @moduledoc """
+  Documentation for `Elixir1brc`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Elixir1brc.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
