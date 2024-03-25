b358ccc094949319e4957c7a261c3a9f311b8cd8
Raj Rajhans <me@rajrajhans.com>
2024-03-25 15:38:52 +0530

    feat: remove Enum.min & Enum.max by maintaining the min, max, sum, count in each iteration
    this took the time taken for 10 mil from 10.6s to 9.3s

--- lib/measurements_processor.ex
+++ lib/measurements_processor.ex
@@ -13,31 +13,63 @@ defmodule OneBRC.MeasurementsProcessor do
 
     Logger.info("***OUTPUT***:\n\n#{inspect(output)}\n\n")
     Logger.info("Processed #{count} rows in #{time_s} s")
+
+    write_result(output, count)
   end
 
   def process_(count) do
     file_path = measurements_file(count)
     {:ok, content} = File.read(file_path)
 
-    acc =
+    t1 = System.monotonic_time(:millisecond)
+
+    result =
       content
       |> String.split("\n")
       |> Enum.map(&String.split(&1, ";"))
       |> Enum.reject(fn value -> value |> Enum.at(0) == "" end)
       |> Enum.reduce(%{}, fn [key, value], acc ->
         {val, _} = Float.parse(value)
-        Map.update(acc, key, [val], fn v -> [val | v] end)
+
+        default = %{
+          min: val,
+          max: val,
+          sum: val,
+          count: 1
+        }
+
+        Map.update(acc, key, default, fn %{
+                                           min: min,
+                                           max: max,
+                                           sum: sum,
+                                           count: count
+                                         } ->
+          min = if val < min, do: val, else: min
+          max = if val > max, do: val, else: max
+          sum = sum + val
+          count = count + 1
+
+          %{
+            min: min,
+            max: max,
+            sum: sum,
+            count: count
+          }
+        end)
       end)
 
+    t2 = System.monotonic_time(:millisecond)
+
     result =
-      acc
-      |> Enum.map(fn {key, values} ->
-        min = Enum.min(values) |> round_to_single_decimal()
-        max = Enum.max(values) |> round_to_single_decimal()
-        mean = (Enum.sum(values) / length(values)) |> round_to_single_decimal()
+      result
+      |> Enum.map(fn {key, %{min: min, max: max, sum: sum, count: count}} ->
+        mean = (sum / count) |> round_to_single_decimal()
 
         {key, %{min: min, max: max, mean: mean}}
       end)
+
+    Logger.info("Processing data 1: #{t2 - t1} ms")
+    Logger.info("Processing data 2: #{System.monotonic_time(:millisecond) - t2} ms")
 
     result
   end
@@ -48,5 +80,23 @@ defmodule OneBRC.MeasurementsProcessor do
 
   defp round_to_single_decimal(number) do
     round(number * 10) / 10.0
+  end
+
+  defp write_result(result, count) do
+    map =
+      result
+      |> Enum.reduce([], fn {key, values}, acc ->
+        acc ++
+          [
+            %{
+              station_name: key,
+              min: values.min,
+              max: values.max,
+              mean: values.mean
+            }
+          ]
+      end)
+
+    File.write!("./data/result_b1.#{count}.json", Jason.encode!(map))
   end
 end
