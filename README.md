8a3785ffafe00baad4da50f300a25055018ac7d5
Raj Rajhans <me@rajrajhans.com>
2024-05-01 15:07:01 +0530

    feat: baseline results generator

--- README.md
+++ README.md
@@ -4,26 +4,31 @@
 
 I took to solve the challenge in two parts - first being to optimize the creation of one billion measurements, and then to actually process them as per the original challenge.
 
-## Part 1: Writing 1 billion measurements to a file
+## Part 1: Writing measurements to a file
 
 - The goal here is to create a file with one billion rows, each row containing a weather station name and a temperature.
 - There are baseline temperatures for each station given, and the temperature for each row is the baseline temperature for that station plus a random number between -10 and 10.
 - The file is a text file with each line of the format `station_name;temperature`.
 - The fastest solution I've implemented so far creates the file with 1 billion measurements in 205 seconds. The code is at [`lib/measurements_generator.ex`](lib/measurements_generator.ex).
 - For more details, check out the [doc](docs/part_1_writing_billion_measurements.md).
-- Commands for creating measurements:
-  - `run create_measurements 1000` - creates a file `./data/measurements.1000.txt` with 1000 measurements.
-  - `run create_measurements.profile 1000` - creates a file `./data/measurements.1000.txt` with 1000 measurements and profiles the execution using `eprof`.
-  - Check `./bin/run` to see what the above commands do.
 
-## Part 2: Processing 1 billion measurements
+### Commands for creating measurements:
+
+- `run create_measurements 1000` - creates a file `./data/measurements.1000.txt` with 1000 measurements. You can change 1000 to the number of measurements you want.
+- `run create_measurements.profile 1000` - creates a file `./data/measurements.1000.txt` with 1000 measurements and profiles the execution using `eprof`.
+- Check `./bin/run` to see what the above commands do.
+
+## Part 2: Processing measurements
 
 - WIP.
 - For more details, check out the [doc](docs/part_2_processing_billion_measurements.md).
-- Commands for processing measurements:
-  - `run process_measurements 1000` - processes the file `./data/measurements.1000.txt` with 1000 measurements.
-  - `run process_measurements.profile 1000` - processes the file `./data/measurements.1000.txt` with 1000 measurements and profiles the execution using `eprof`.
-  - Check `./bin/run` to see what the above commands do.
+
+### Commands for processing measurements:
+
+- `run create_baseline_results 1000` - creates a file `./data/result_baseline.10000.json` with the correct results for 1000 measurements. This file can then be used to verify the correctness of results we get while trying to optimize the processing.
+- `run process_measurements 1000` - processes the file `./data/measurements.1000.txt` with 1000 measurements, generates `result.10000.json` & verifies their correctness with the file `./data/result_baseline.10000.json`.
+- `run process_measurements.profile 1000` - processes the file `./data/measurements.1000.txt` with 1000 measurements and profiles the execution using `eprof`.
+- Check `./bin/run` to see what the above commands do.
 
 ## Setting up this repo
 
---
+++ lib/baseline_results_generator.ex
@@ -0,0 +1,133 @@
+defmodule OneBRC.BaselineResultsGenerator do
+  @moduledoc """
+  Uses a known correct way to generate baseline results for the measurements processor, that can be used to test the correctness of the optimized versions.
+  """
+  @measurements_file "./data/measurements.{COUNT}.txt"
+  @count 1_000_000_000
+  require Logger
+
+  def process, do: process(@count)
+
+  def process(count) do
+    Logger.info("Processing measurements")
+
+    {time, output} = :timer.tc(fn -> process_(count) end)
+    time_s = round(time / 1_000_000 * 10) / 10.0
+
+    Logger.info("***OUTPUT***:\n\n#{inspect(output)}\n\n")
+    Logger.info("Processed #{count} rows in #{time_s} s")
+
+    write_result(output, count)
+  end
+
+  def process_(count) do
+    file_path = measurements_file(count)
+    fs = File.stream!(file_path)
+
+    t1 = System.monotonic_time(:millisecond)
+
+    ets_table = :ets.new(:station_stats, [:set, :public])
+
+    fs
+    |> Stream.chunk_every(1000)
+    |> Task.async_stream(
+      fn val -> Enum.map(val, &parse_row/1) end,
+      max_concurrency: System.schedulers_online() * 5,
+      ordered: false,
+      timeout: :infinity
+    )
+    |> Stream.flat_map(&elem(&1, 1))
+    |> Stream.map(&process_row(&1, ets_table))
+    |> Stream.run()
+
+    t2 = System.monotonic_time(:millisecond)
+
+    result =
+      :ets.tab2list(ets_table)
+      |> Enum.map(fn {key, %{min: min, max: max, sum: sum, count: count}} ->
+        mean = (sum / (count * 10.0)) |> round_to_single_decimal()
+
+        {key, %{min: min / 10.0, max: max / 10.0, mean: mean}}
+      end)
+
+    Logger.info("Processing data 1: #{t2 - t1} ms")
+    Logger.info("Processing data 2: #{System.monotonic_time(:millisecond) - t2} ms")
+
+    result
+  end
+
+  defp parse_row(row) do
+    case row do
+      "" ->
+        nil
+
+      row ->
+        [key, value] = :binary.split(row, ";")
+
+        parsed_value =
+          value |> String.trim_trailing()
+
+        [a, b] = parsed_value |> :binary.split(".")
+        parsed_value = (a <> b) |> String.to_integer()
+
+        [key, parsed_value]
+    end
+  end
+
+  defp process_row([key, val], ets_table) do
+    existing_record = :ets.lookup(ets_table, key)
+
+    new_record =
+      case existing_record do
+        [] ->
+          %{
+            min: val,
+            max: val,
+            sum: val,
+            count: 1
+          }
+
+        [{^key, record}] ->
+          min = if val < record.min, do: val, else: record.min
+          max = if val > record.max, do: val, else: record.max
+          sum = record.sum + val
+          count = record.count + 1
+
+          %{
+            min: min,
+            max: max,
+            sum: sum,
+            count: count
+          }
+      end
+
+    :ets.insert(ets_table, {key, new_record})
+  end
+
+  defp measurements_file(count) do
+    String.replace(@measurements_file, "{COUNT}", Integer.to_string(count))
+  end
+
+  defp round_to_single_decimal(number) do
+    round(number * 10) / 10.0
+  end
+
+  defp write_result(result, count) do
+    map =
+      result
+      |> Enum.reduce([], fn {key, values}, acc ->
+        acc ++
+          [
+            %{
+              station_name: key,
+              min: values.min,
+              max: values.max,
+              mean: values.mean
+            }
+          ]
+      end)
+
+    baseline_file_path = "./data/result_baseline.#{count}.json"
+    File.write!(baseline_file_path, Jason.encode!(map))
+  end
+end
