03731d0ec51bd487a4c29310ea423a2b40c6522d
Raj Rajhans <me@rajrajhans.com>
2024-05-04 17:51:15 +0530

    feat: add concurrency to process step
    - instead of processing each line one by one, we give a chunk of lines to parallel workers. each worker will just process that chunk, assuming there's no other chunk
    - uses ets as a "accumulator" for the parallel workers to write their own results to
    - at the end, after all the chunks are done processing by parallel workers, we do one more pass to fold all the individual results into one, which will be the final result
    - shaves off around 0.2 - 0.3 seconds

--- lib/one_brc/measurements_processor.ex
+++ lib/one_brc/measurements_processor.ex
@@ -26,25 +26,86 @@ defmodule OneBRC.MeasurementsProcessor do
     ets_table = :ets.new(:station_stats, [:set, :public])
 
     fs
-    |> Stream.chunk_every(1000)
+    |> Stream.chunk_every(2000)
     |> Task.async_stream(
       fn val -> Enum.map(val, &parse_row/1) end,
-      max_concurrency: System.schedulers_online() * 5,
+      max_concurrency: System.schedulers_online(),
       ordered: false,
       timeout: :infinity
     )
-    |> Stream.flat_map(&elem(&1, 1))
-    |> Stream.map(&process_row(&1, ets_table))
+    |> Stream.with_index()
+    |> Task.async_stream(
+      fn {{:ok, parsed_rows}, row_index} ->
+        interim_records =
+          Enum.reduce(parsed_rows, %{}, fn row, acc ->
+            process_row(row, acc)
+          end)
+
+        :ets.insert(ets_table, {row_index, interim_records})
+      end,
+      max_concurrency: System.schedulers_online(),
+      ordered: false,
+      timeout: :infinity
+    )
     |> Stream.run()
 
     t2 = System.monotonic_time(:millisecond)
 
     result =
       :ets.tab2list(ets_table)
-      |> Enum.map(fn {key, %{min: min, max: max, mean: mean}} ->
-        mean = (mean / 10.0) |> round_to_single_decimal()
+      |> Enum.map(fn {_row_idx, records} ->
+        records
+      end)
+      |> Enum.reduce(%{}, fn interim_records_map, super_acc ->
+        aggregated_records_for_current_row =
+          Enum.reduce(interim_records_map, %{}, fn {key, interim_record}, temp_acc ->
+            existing_aggregated_record = Map.get(super_acc, key, nil)
 
-        {key, %{min: min / 10.0, max: max / 10.0, mean: mean}}
+            new_aggregated_record =
+              case existing_aggregated_record do
+                nil ->
+                  interim_record
+
+                aggregated_record ->
+                  min =
+                    if interim_record.min < aggregated_record.min,
+                      do: interim_record.min,
+                      else: aggregated_record.min
+
+                  max =
+                    if interim_record.max > aggregated_record.max,
+                      do: interim_record.max,
+                      else: aggregated_record.max
+
+                  mean =
+                    (aggregated_record.mean * aggregated_record.count +
+                       interim_record.mean * interim_record.count) /
+                      (aggregated_record.count + interim_record.count)
+
+                  count = aggregated_record.count + interim_record.count
+
+                  %{
+                    min: min,
+                    max: max,
+                    mean: mean,
+                    count: count
+                  }
+              end
+
+            Map.put(temp_acc, key, new_aggregated_record)
+          end)
+
+        Map.merge(super_acc, aggregated_records_for_current_row)
+      end)
+      |> Enum.map(fn {key, value} ->
+        mean = (value.mean / 10.0) |> round_to_single_decimal()
+
+        {key,
+         %{
+           min: round_to_single_decimal(value.min / 10.0),
+           max: round_to_single_decimal(value.max / 10.0),
+           mean: mean
+         }}
       end)
 
     Logger.info("Processing data 1: #{t2 - t1} ms")
@@ -71,12 +132,12 @@ defmodule OneBRC.MeasurementsProcessor do
     end
   end
 
-  defp process_row([key, val], ets_table) do
-    existing_record = :ets.lookup(ets_table, key)
+  defp process_row([key, val], acc) do
+    existing_record = Map.get(acc, key, nil)
 
     new_record =
       case existing_record do
-        [] ->
+        nil ->
           %{
             min: val,
             max: val,
@@ -84,20 +145,21 @@ defmodule OneBRC.MeasurementsProcessor do
             count: 1
           }
 
-        [{^key, record}] ->
-          min = if val < record.min, do: val, else: record.min
-          max = if val > record.max, do: val, else: record.max
-          mean = (record.mean * record.count + val) / (record.count + 1)
+        %{count: count, min: min, max: max, mean: mean} ->
+          min = if val < min, do: val, else: min
+          max = if val > max, do: val, else: max
+
+          mean = (mean * count + val) / (count + 1)
 
           %{
             min: min,
             max: max,
             mean: mean,
-            count: record.count + 1
+            count: count + 1
           }
       end
 
-    :ets.insert(ets_table, {key, new_record})
+    Map.put(acc, key, new_record)
   end
 
   defp measurements_file(count) do
