2623e619891b123783201493aaf318169730e08e
Raj Rajhans <me@rajrajhans.com>
2024-09-06 23:40:45 +0530

    fix: versions

--- lib/one_brc/versions/version_3.ex
+++ lib/one_brc/versions/version_3.ex
@@ -15,83 +15,22 @@ defmodule OneBRC.MeasurementsProcessor.Version3 do
     |> Stream.chunk_every(10000)
     |> Task.async_stream(
       fn val -> Enum.map(val, &parse_row/1) end,
-      max_concurrency: System.schedulers_online(),
+      max_concurrency: System.schedulers_online() * 5,
       ordered: false,
       timeout: :infinity
     )
-    |> Stream.with_index()
-    |> Task.async_stream(
-      fn {{:ok, parsed_rows}, row_index} ->
-        interim_records =
-          Enum.reduce(parsed_rows, %{}, fn row, acc ->
-            process_row(row, acc)
-          end)
-
-        :ets.insert(ets_table, {row_index, interim_records})
-      end,
-      max_concurrency: System.schedulers_online(),
-      ordered: false,
-      timeout: :infinity
-    )
+    |> Stream.flat_map(&elem(&1, 1))
+    |> Stream.map(&process_row(&1, ets_table))
     |> Stream.run()
 
     t2 = System.monotonic_time(:millisecond)
 
     result =
       :ets.tab2list(ets_table)
-      |> Enum.map(fn {_row_idx, records} ->
-        records
-      end)
-      |> Enum.reduce(%{}, fn interim_records_map, super_acc ->
-        aggregated_records_for_current_row =
-          Enum.reduce(interim_records_map, %{}, fn {key, interim_record}, temp_acc ->
-            existing_aggregated_record = Map.get(super_acc, key, nil)
+      |> Enum.map(fn {key, %{min: min, max: max, sum: sum, count: count}} ->
+        mean = (sum / (count * 10.0)) |> round_to_single_decimal()
 
-            new_aggregated_record =
-              case existing_aggregated_record do
-                nil ->
-                  interim_record
-
-                aggregated_record ->
-                  min =
-                    if interim_record.min < aggregated_record.min,
-                      do: interim_record.min,
-                      else: aggregated_record.min
-
-                  max =
-                    if interim_record.max > aggregated_record.max,
-                      do: interim_record.max,
-                      else: aggregated_record.max
-
-                  mean =
-                    (aggregated_record.mean * aggregated_record.count +
-                       interim_record.mean * interim_record.count) /
-                      (aggregated_record.count + interim_record.count)
-
-                  count = aggregated_record.count + interim_record.count
-
-                  %{
-                    min: min,
-                    max: max,
-                    mean: mean,
-                    count: count
-                  }
-              end
-
-            Map.put(temp_acc, key, new_aggregated_record)
-          end)
-
-        Map.merge(super_acc, aggregated_records_for_current_row)
-      end)
-      |> Enum.map(fn {key, value} ->
-        mean = (value.mean / 10.0) |> round_to_single_decimal()
-
-        {key,
-         %{
-           min: round_to_single_decimal(value.min / 10.0),
-           max: round_to_single_decimal(value.max / 10.0),
-           mean: mean
-         }}
+        {key, %{min: min / 10.0, max: max / 10.0, mean: mean}}
       end)
 
     t3 = System.monotonic_time(:millisecond)
@@ -130,34 +69,34 @@ defmodule OneBRC.MeasurementsProcessor.Version3 do
     end
   end
 
-  defp process_row([key, val], acc) do
-    existing_record = Map.get(acc, key, nil)
+  defp process_row([key, val], ets_table) do
+    existing_record = :ets.lookup(ets_table, key)
 
     new_record =
       case existing_record do
-        nil ->
+        [] ->
           %{
             min: val,
             max: val,
-            mean: val,
+            sum: val,
             count: 1
           }
 
-        %{count: count, min: min, max: max, mean: mean} ->
-          min = if val < min, do: val, else: min
-          max = if val > max, do: val, else: max
-
-          mean = (mean * count + val) / (count + 1)
+        [{^key, record}] ->
+          min = if val < record.min, do: val, else: record.min
+          max = if val > record.max, do: val, else: record.max
+          sum = record.sum + val
+          count = record.count + 1
 
           %{
             min: min,
             max: max,
-            mean: mean,
-            count: count + 1
+            sum: sum,
+            count: count
           }
       end
 
-    Map.put(acc, key, new_record)
+    :ets.insert(ets_table, {key, new_record})
   end
 
   defp round_to_single_decimal(number) do
